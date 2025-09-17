a8391c22b7a16b2ed1e83603c021108073c1efa4
Marcos <Marcos.OliveiraDEV@gmail.com>
2025-09-17 02:07:05 +0000

    feat: create databse connection with knex

--- src/env/index.js
+++ src/env/index.js
@@ -4,6 +4,11 @@ import z from "zod";
 const envSchema = z.object({
   ENV: z.enum(["development", "test", "production"]).default("production"),
   PORT: z.coerce.number().default(3000),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number(),
+  POSTGRES_DB: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
