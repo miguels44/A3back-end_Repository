ba51e889912117f7cf51d8661c2ab52a131319f0
fernanda <fernandapelizonz@hotmail.com>
2025-10-13 18:43:24 +0000

    Criação do migration de subjects

---
+++ infra/migrations/20251007194453_create_subjects.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema.createTable('subjects', (table) => {
+    
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+
+    table.string('name', 100).notNullable().unique();
+
+    table.text('description');
+
+    table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema.dropTable('subjects');
+};
